b0514ece965e06930bcc090c26999bc0f82b20d5
João Dini <copperbrjdmdini@gmail.com>
2022-08-17 13:24:58 -0300

    `IndexController` e `.gitignore`

---
+++ src/main/java/br/ufscar/dc/pooa/controller/IndexController.java
@@ -0,0 +1,51 @@
+package main.java.br.ufscar.dc.pooa.controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/index.jsp")
+public class IndexController extends HttpServlet {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        
+		System.out.println("== [LOG]: IndexController");
+
+        String action = request.getPathInfo();
+        if (action == null) {
+            action = "";
+        }
+
+        try {
+            switch (action) {
+                default:
+                    lista(request, response);
+            }
+        } catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private void lista(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/inicial.jsp");
+        dispatcher.forward(request, response);
+    }
+}
