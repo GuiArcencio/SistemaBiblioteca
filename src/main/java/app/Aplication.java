9276c5210997be090290ce8e8c4ceb3c46023e98
João Dini <copperbrjdmdini@gmail.com>
2022-08-30 10:27:38 -0300

    Base API `ControllerObras`

--- src/main/java/app/Aplication.java
+++ src/main/java/app/Aplication.java
@@ -2,6 +2,7 @@ package app;
 import static spark.Spark.*;
 
 import app.Autor.ControllerAutor;
+import app.Obra.ControllerObras;
 
 public class Aplication {
 
@@ -10,9 +11,28 @@ public class Aplication {
         path("/api", () -> {
             before("/*", (q, a) -> System.out.println("Chamada API recebida"));
             
+            // Essas rotas foram utilizadas para teste
             post("/autor",      ControllerAutor.insere);
             get("/autor",       ControllerAutor.getAutores);
             get("/autor/:nome", ControllerAutor.getAutor);
+
+            // Rotas do ControllerObras
+            // // Busca todas as Obras
+            // get("/obra", ControllerObras.buscaObras);
+            // // Busca uma obra por ISBN
+            // get("/obra/:isbn", ControllerObras.buscaObra);
+            // // Adiciona uma obra
+            // post("/obra", ControllerObras.adicionarObra);
+            // // Remove uma obra por ISBN
+            // delete("/obra/:isbn", ControllerObras.removerObra);
+            // // Adiciona um autor
+            // post("/autor", ControllerObras.adicionarAutor);
+            // // Altera um autor
+            // put("/autor/:nome", ControllerObras.alterarAutor);
+            // // Remove um autor por nome
+            // delete("/autor/:nome", ControllerObras.removerAutor);
+            // // Altera disponibilidade de uma cópia por código
+            // put("/copia/:codigo/disponibilidade", ControllerObras.mudarDisponibilidadeCopia);
         });
     }
 }
