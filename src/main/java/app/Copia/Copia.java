93d1efc972e713b0aca0e5dc02ada7b43fc67f5d
fernando <fernandooliveira@estudante.ufscar.br>
2022-08-31 00:34:23 -0300

    adicao do CopiaDAO

--- src/main/java/app/Copia/Copia.java
+++ src/main/java/app/Copia/Copia.java
@@ -2,16 +2,46 @@ package app.Copia;
 import app.Estados.*;
 
 public class Copia {
-	int id;
-	State state;
+	private int id;
+	private State state;
+    private Long obraId;
 	
 	public Copia(int id, State state) {
 		this.id = id;
 		this.state = state;
 	}
+
+    public Copia(int id, State state, Long obraId) {
+        this(id, state);
+        this.obraId = obraId;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public State getState() {
+        return this.state;
+    }
+
+    public void setObraId(Long obraID){
+        this.obraId = obraId;
+    }
+
+    public Long getObraId() {
+        return this.obraId;
+    }
 	
 	public void emprestar() {
-		
+        
 	}
 	
 	public void devolver() {
@@ -25,4 +55,6 @@ public class Copia {
 	public State estadoAtual() {
 		return this.state;
 	}
+
+    
 }
---
+++ src/main/java/app/dao/CopiaDAO.java
@@ -0,0 +1,114 @@
+package app.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+
+import app.Copia.Copia;
+import app.Estados.*;
+
+
+public class CopiaDAO extends GenericDAO{
+
+    public void insert(Copia copia){
+        String sql = "INSERT INTO Copia (state, obra_id) VALUES (?, ?) ";
+
+        try{
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, copia.getState().getState());
+            statement.setLong(2, copia.getObraId());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(Copia copia) {
+        String sql = "UPDATE Copia SET state = ?, obra_id = ? WHERE id = ?";
+
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, copia.getState().getState());
+            statement.setLong(2, copia.getObraId());
+            statement.setInt(3, copia.getId());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void delete(Copia copia){
+        String sql = "DELETE FROM Copia where id = ?";
+
+        try{
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setInt(1, copia.getId());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public Copia getById(int id){
+        Copia copia = null;
+
+        String sql = "SELECT * from Copia WHERE id = ?";
+
+        try{
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                String statestr = resultSet.getString("state");
+                Long obraId = resultSet.getLong("obra_id");
+            
+                State state = null;
+
+                if (statestr == "Emprestado"){
+                    state = Emprestado.getInstancia();
+                }
+
+                else if(statestr == "Reservado") {
+                    state = Reservado.getInstancia();
+                }
+            
+                else {
+                    state = Disponivel.getInstancia();
+                }
+
+                copia = new Copia(id, state, obraId);
+            }
+            resultSet.close();
+            statement.close();
+            conn.close();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return copia;
+
+    }
+
+}
+
