5bbb43f6786c27aedafce3f7b050e455bb1ea9a2
Mobuos <copperbrjdmdini@gmail.com>
2022-08-31 19:49:23 -0300

    Base do serviço de empréstimo

--- src/main/java/app/Service/impl/EmprestimoService.java
+++ src/main/java/app/Service/impl/EmprestimoService.java
@@ -1,7 +1,52 @@
 package app.Service.impl;
 
+import java.math.BigInteger;
+import java.util.List;
+
+import app.Domain.PacoteEntradaSaidaObras.Devolucao;
+import app.Domain.PacoteEntradaSaidaObras.Emprestimo;
+import app.Domain.PacoteObras.Copia;
 import app.Service.spec.IEmprestimoService;
+import app.dao.EmprestimoDAO;
+import app.dao.UsuarioDAO;
 
 public class EmprestimoService implements IEmprestimoService {
+
+    private EmprestimoDAO eDao;
+    private UsuarioDAO uDao;
+
+    public EmprestimoService() {
+        this.dao = new EmprestimoDAO();
+    }
+
+    @Override
+    public List<Emprestimo> buscaEmprestimos(BigInteger idUsuario) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Devolucao> buscaDevolucoes(BigInteger idUsuario) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Copia emprestarObra(BigInteger idUsuario, BigInteger isbn) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Copia devolverObra(BigInteger idUsuario, BigInteger isbn) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Copia reservarObra(BigInteger idUsuario, BigInteger isbn) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
