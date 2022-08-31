9c62db2dad9df1ef845c12bb5dcc392933461b26
enio-martinelli <eniocmartinelli@gmail.com>
2022-08-31 01:30:21 -0300

    .>..

--- src/main/java/app/index/IndexController.java
+++ src/main/java/app/index/IndexController.java
@@ -1,14 +1,51 @@
-package main.java.app.index;
+package main.java.app.Index;
 
-import main.java.app.util.*;
-import spark.*;
-import java.util.*;
-import static main.java.app.Application.*;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class IndexController {
-    public static Route serveIndexPage = (Request request, Response response) -> {
-        Map<String, Object> model = new HashMap<>();
-        model.put("users", usuarioDao.getAllUserNames());
-        return ViewUtil.render(request, model, Path.Template.INDEX);
-    };
+@WebServlet(urlPatterns = "/index.jsp")
+public class IndexController extends HttpServlet {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        
+		System.out.println("== [LOG]: IndexController");
+
+        String action = request.getPathInfo();
+        if (action == null) {
+            action = "";
+        }
+
+        try {
+            switch (action) {
+                default:
+                    lista(request, response);
+            }
+        } catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private void lista(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/inicial.jsp");
+        dispatcher.forward(request, response);
+    }
 }
