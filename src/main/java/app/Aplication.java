6222b374394c2e5071f5754bf6161682786f962f
enio-martinelli <eniocmartinelli@gmail.com>
2022-09-17 19:07:12 -0300

    endereço

---
+++ src/main/java/app/Aplication.java
@@ -0,0 +1,68 @@
+package app;
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+
+import app.Controllers.ControllerEmprestimo;
+import app.Controllers.ControllerObras;
+import app.Exception.ControllerException;
+import app.Controllers.ControllerCategoriaLeitor;
+import app.Controllers.ControllerUsuario;
+import app.Controllers.ControllerEndereco;
+
+public class Aplication {
+
+    public static void main(String[] args) {
+
+        Gson gson = new Gson();
+
+        path("/api", () -> {
+            before("/*", (q, a) -> System.out.println("Chamada API recebida"));
+
+            // Rotas do ControllerObras
+            get("/obra/:codigo",             ControllerObras.buscaObra, gson::toJson);
+            post("/obra",                  ControllerObras.adicionarObra, gson::toJson);
+            delete("/obra/:codigo",          ControllerObras.removerObra, gson::toJson);
+            post("/obra/:codigo/autor",      ControllerObras.adicionarAutor, gson::toJson);
+            delete("/obra/:codigo/autor",    ControllerObras.removerAutor, gson::toJson);
+            post("/obra/:codigo/palavras",   ControllerObras.adicionarPalavraChave, gson::toJson);
+            delete("/obra/:codigo/palavras", ControllerObras.removerPalavraChave, gson::toJson);
+            // put("/copia/:codigo/disp",     ControllerObras.mudarDisponibilidadeCopia, gson::toJson);
+
+            // Rotas de ControllerEmprestimo
+            // get("/usuario/:id/emprestimo",        ControllerEmprestimo.consultarEmprestimo, gson::toJson);
+            // get("/usuario/:id/devolucao",         ControllerEmprestimo.consultarDevolucao, gson::toJson);
+            // patch("/usuario/:id/emprestimo/:isbn",ControllerEmprestimo.emprestarObra, gson::toJson);
+            // patch("/usuario/:id/reserva/:isbn",   ControllerEmprestimo.reservarObra, gson::toJson);
+            // patch("/usuario/:id/devolucao/:isbn", ControllerEmprestimo.devolverObra, gson::toJson);
+
+            // Rotas de ControllerCategoriaLeitor
+            get("/categoria/leitor",      ControllerCategoriaLeitor.getCategorias, gson::toJson);
+            post("/categoria/leitor",     ControllerCategoriaLeitor.criarCategoria, gson::toJson);
+            delete("/categoria/leitor/:id", ControllerCategoriaLeitor.removerCategoria, gson::toJson);
+            put("/categoria/leitor/:id",  ControllerCategoriaLeitor.alterarCategoria, gson::toJson);
+
+            //Rotas de ControllerUsuario
+            post("/usuario/:id/leitor", ControllerUsuario.criarLeitor, gson::toJson);
+            post("/usuario/:id/funcionario", ControllerUsuario.criarFuncionario, gson::toJson);
+            delete("/usuario/:id/leitor", ControllerUsuario.removerLeitor, gson::toJson);
+            delete("/usuario/:id/funcionario", ControllerUsuario.removerFuncionario, gson::toJson);
+            put("/usuario/:id/leitor", ControllerUsuario.alterarLeitor, gson::toJson);
+            put("/usuario/:id/leitor", ControllerUsuario.alterarFuncionario, gson::toJson);
+
+            //Rotas de Endereço
+            get("/enderecos", ControllerEndereco.getEnderecos, gson::toJson);
+            get("/endereco/:id", ControllerEndereco.getEndereco, gson::toJson);
+            post("/endereco", ControllerEndereco.criarEndereco, gson::toJson);
+            delete("/endereco", ControllerEndereco.removerEndereco, gson::toJson);
+            put("/endereco/:id", ControllerEndereco.alterarEndereco, gson::toJson);
+            
+            // Exceptions
+            exception(NumberFormatException.class, ControllerException.numberFormatException);
+            exception(JsonSyntaxException.class, ControllerException.jsonSyntaxException);
+            exception(JsonParseException.class, ControllerException.jsonParseException);
+        });
+    }
+}
---
+++ src/main/java/app/Domain/PacoteUsuarios/Endereco.java
@@ -0,0 +1,85 @@
+package app.Domain.PacoteUsuarios;
+
+import app.Exception.AnnotatedDeserializer.JsonRequired;
+
+public class Endereco {
+    private Long id;
+
+    @JsonRequired
+    private String logradouro;
+
+    @JsonRequired
+    private int numero;
+
+    @JsonRequired
+    private int cep;
+
+    @JsonRequired
+    private String cidade;
+
+    @JsonRequired
+    private String estado;
+
+    public Endereco(Long id, String logradouro, int numero, int cep, String cidade, String estado) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    
+    public Endereco() {
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+    
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getLogradouro() {
+        return this.logradouro;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public int getCep() {
+        return this.cep;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+}
